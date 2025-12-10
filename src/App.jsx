53d062fe1668310398f71528e6e7398088b06fbd
mattedgar97 <matthew.edgar@huler.io>
2025-12-10 09:45:37 +0000

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">F3RG13</h1>
+      <p className="subtitle">DJ & Music Producer</p>
+      <p className="coming-soon">Coming Soon...</p>
+    </div>
+  )
+}
+
+export default App
